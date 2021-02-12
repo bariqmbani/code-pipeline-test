d3966740ef291718a85cded4f4cf8f189eede744
Bariq Mbani <bariq.m70@gmail.com>
2021-02-12 21:46:49 +0700

    change message

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('hello world 2!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
